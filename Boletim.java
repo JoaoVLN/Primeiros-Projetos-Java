39152b2d0fb416ecc6f1a4f1973f6b43d96ae3c5
JoaoVLN <142940856+JoaoVLN@users.noreply.github.com>
2023-08-22 17:59:35 -0300

    Add files via upload

---
+++ Boletim.java
@@ -0,0 +1,11 @@
+package Boletim;
+
+public class Boletim{
+    public static void main(String[] args) {
+        int media = 5;
+        if (media < 6);
+        System.out.println("Reprovado");
+        else if (media > 6);
+        System.out.println("Aprovado");
+    }
+}
